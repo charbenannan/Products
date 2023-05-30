5d2708253dadbc87012fa3937c92996c69f214fe
Charles  Bentum-Annan <79229028+charbenannan@users.noreply.github.com>
2023-05-30 18:45:50 +0100

    Update index.php

--- src/PHP/index.php
+++ src/PHP/index.php
@@ -6,25 +6,18 @@ header("Access-Control-Allow-Methods: POST, GET, OPTIONS, PUT, DELETE");
 
 require_once 'autoload.php';
 
-use classes\ProductController;
+$method = $_SERVER['REQUEST_METHOD'];
 
-switch ($_SERVER['REQUEST_METHOD']) {
-    case 'POST':
-        $productController = new ProductController();
-        $productController->addProduct();
-        break;
-    case 'GET':
-        $productController = new ProductController();
-        $productController->getAllProducts();
-        break;
-    case 'DELETE':
-        $productController = new ProductController();
-        $productController->deleteProduct();
-        break;
-    default:
-        $response = ['status' => 0, 'message' => 'Invalid request method'];
-        echo json_encode($response);
-        break;
-}
+$routes = [
+    'POST' => 'addProduct',
+    'GET' => 'getAllProducts',
+    'DELETE' => 'deleteProduct',
+];
 
+$action = $routes[$method] ?? null;
+
+$productController = new ProductController();
+
+$method = new ReflectionMethod($productController, $action);
+$method->invoke($productController);
 ?>
